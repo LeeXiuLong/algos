0b681292aec131f89be2942908e04c23e2ff5dcf
Jourdan Ooi <jungterooi@gmail.com>
2022-12-15 18:12:23 -0600

    feat(LinkedList): Add linked list kth node algo

---
+++ remove_kth_node.rb
@@ -0,0 +1,17 @@
+def remove_kth_node(head, k)
+    firstPointer = head
+    secondPointer = head
+    counter = 0
+    while counter < k
+        secondPointer = secondPointer.next
+        counter += 1
+    end
+    if !secondPointer
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    while secondPointer.next != nil
+        firstPointer = firstPointer.next
+        secondPointer = secondPointer.next
+    end
+    firstPointer.next = firstPointer.next.next
