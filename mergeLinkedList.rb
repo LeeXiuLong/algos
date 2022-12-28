3f9594ac55f339dc6fb0581959ff0c35ae707808
Jourdan Ooi <jungterooi@gmail.com>
2022-12-28 10:19:04 -0600

    feat(MergeLinkedList): Merge a linked list

---
+++ mergeLinkedList.rb
@@ -0,0 +1,36 @@
+def mergeLinkedList(linkedList1, linkedList2)
+    node1 = linkedList1
+    node2 = linkedList2
+    dict1 = {}
+    dict2 = {}
+    i = 0
+    while node1 and node2
+        if node1
+            if findCommonNode(node1, dict)
+                return findCommonNode(node1, dict)
+            else
+                dict1[i] = node1
+                node1 = node1.next
+            end
+        end
+        if node2
+            if findCommonNode(node2, dict)
+                return findCommonNode(node2, dict)
+            else
+                dict2[i] = node2
+                node2 = node2.next
+            end
+        end
+        i += 1
+    end
+    return nil
+end
+
+def findCommonNode(node, dict)
+    dict.each do |k, v|
+        if node = v
+            return v
+        end
+    end
+    return nil
+end
