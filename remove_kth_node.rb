ae70dd5c40196f86659d797e0396672404826559
Ooi <jungterooi@gmail.com>
2023-06-09 13:23:36 -0500

    chore(example): Delete code

--- remove_kth_node.rb
+++ remove_kth_node.rb
@@ -10,11 +10,6 @@ def remove_kth_node(head, k)
         head.value = head.next.value
         head.next = head.next.next
         return
-    while secondPointer.next != nil
-        firstPointer = firstPointer.next
-        secondPointer = secondPointer.next
-    end
-    firstPointer.next = firstPointer.next.next
 #asdfaasdfasdfb
 
 #this is a new example change
